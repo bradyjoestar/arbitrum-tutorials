7df13cbcc1014ce08476a09570a95ffeb7ae6988
wenbin <wenbin.shi@gmail.com>
2022-09-14 15:35:34 +0800

    update withdraw

--- packages/eth-withdraw/scripts/exec.js
+++ packages/eth-withdraw/scripts/exec.js
@@ -1,5 +1,5 @@
 const { utils, providers, Wallet } = require('ethers')
-const { EthBridger, getL2Network, L2ToL1Message } = require('@arbitrum/sdk')
+const { EthBridger, getL2Network, addCustomNetwork,L2ToL1Message } = require('@arbitrum/sdk')
 const { parseEther } = utils
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
 require('dotenv').config()
@@ -20,6 +20,54 @@ const ethFromL2WithdrawAmount = parseEther('0.000001')
 const main = async () => {
   await arbLog('Withdraw Eth via Arbitrum SDK')
 
+  await addCustomNetwork({
+    customL1Network: {
+      "blockTime": 10,
+      "chainID": 1337,
+      "explorerUrl": "",
+      "isCustom": true,
+      "name": "EthLocal",
+      "partnerChainIDs": [
+        412346
+      ],
+      "rpcURL": "http://localhost:8545"
+    },
+    customL2Network: {
+      "chainID": 412346,
+      "confirmPeriodBlocks": 20,
+      "ethBridge": {
+        "bridge": "0xdd041cddc4ea2655627d9d907e01f40490c3bd89",
+        "inbox": "0xdc8bb4dad70459f076f3484cc8c43722cae36a01",
+        "outbox": "0xb6bE947DCA31b096Dc1041aC30e806A6F13Bf871",
+        "rollup": "0xc2add8f204679376f36c16cf3b9f588b8551bc83",
+        "sequencerInbox": "0xcf7feadc1e76f98ebd3a5d00192002069463fb35"
+      },
+      "explorerUrl": "",
+      "isArbitrum": true,
+      "isCustom": true,
+      "name": "ArbLocal",
+      "partnerChainID": 1337,
+      "rpcURL": "http://localhost:8547",
+      "retryableLifetimeSeconds": 604800,
+      "tokenBridge": {
+        "l1CustomGateway": "0xDe67138B609Fbca38FcC2673Bbc5E33d26C5B584",
+        "l1ERC20Gateway": "0x0Bdb0992B3872DF911260BfB60D72607eb22d5d4",
+        "l1GatewayRouter": "0x4535771b8D5C43100f126EdACfEc7eb60d391312",
+        "l1MultiCall": "0x36BeF5fD671f2aA8686023dE4797A7dae3082D5F",
+        "l1ProxyAdmin": "0xF7818cd5f5Dc379965fD1C66b36C0C4D788E7cDB",
+        "l1Weth": "0x24067223381F042fF36fb87818196dB4D2C56E9B",
+        "l1WethGateway": "0xBa3d12E370a4b592AAF0CA1EF09971D196c27aAd",
+        "l2CustomGateway": "0xF0B003F9247f2DC0e874710eD55e55f8C63B14a3",
+        "l2ERC20Gateway": "0x78a6dC8D17027992230c112432E42EC3d6838d74",
+        "l2GatewayRouter": "0x7b650845242a96595f3a9766D4e8e5ab0887936A",
+        "l2Multicall": "0x9b890cA9dE3D317b165afA7DFb8C65f2e4c95C20",
+        "l2ProxyAdmin": "0x7F85fB7f42A0c0D40431cc0f7DFDf88be6495e67",
+        "l2Weth": "0x36BeF5fD671f2aA8686023dE4797A7dae3082D5F",
+        "l2WethGateway": "0x2E76efCC2518CB801E5340d5f140B1c1911b4F4B"
+      }
+    }
+  })
+
   /**
    * Use l2Network to create an Arbitrum SDK EthBridger instance
    * We'll use EthBridger for its convenience methods around transferring ETH from L2 to L1
@@ -27,6 +75,8 @@ const main = async () => {
 
   const l2Network = await getL2Network(l2Provider)
   const ethBridger = new EthBridger(l2Network)
+
+  console.log(l2Network)
 
   /**
    * First, let's check our L2 wallet's initial ETH balance and ensure there's some ETH to withdraw
